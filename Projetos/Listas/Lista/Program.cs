502b59060910159d15403716de1a7213dfba753a
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2023-11-15 22:21:53 -0300

    Exercício de fixação de Listas.

--- Projetos/Listas/Lista/Program.cs
+++ Projetos/Listas/Lista/Program.cs
@@ -18,6 +18,38 @@ namespace Lista
                 Console.WriteLine(nome);
             }
             Console.WriteLine(list.Count);
+
+            list.Remove("Alex");
+
+            string s1 = list.Find(x => x[0] == 'A');
+            string s2 = list.FindLast(x => x[0] == 'A');
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+
+            Console.WriteLine($"First 'A': {s1}");
+            Console.WriteLine($"Last 'A': {s2}");
+            
+            Console.WriteLine($"First pos 'A': {pos1}");
+            Console.WriteLine($"Last pos 'A': {pos2}");
+
+            List<string> list2 = list.FindAll(l => l.Length == 5);
+            Console.WriteLine("*********************************");
+            foreach(string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+            list.Remove("Alex");
+            Console.WriteLine("*********************************");
+            foreach(string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            list.RemoveAll(s => s[0] == 'M');
+            Console.WriteLine("*********************************");
+            foreach(string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
