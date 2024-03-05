0b24e2495b995480cf63b2d62ae2af74b8a41df1
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2024-03-05 07:11:39 -0300

    Estudos sobre composições

---
+++ Projetos/Enum_Composition/Course/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
