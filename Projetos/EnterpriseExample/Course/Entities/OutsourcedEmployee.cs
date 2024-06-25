eb47b4db74ab08eb1a225cb47ebd6dc5648eff99
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2024-06-24 23:24:20 -0300

    Estudo sobre polimorfismo + Atualização de repositório

---
+++ Projetos/EnterpriseExample/Course/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+﻿namespace Course.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(double additionalCharge, string name, int hours, double valuePerHour)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            const double PERCENT_ADDITIONAL = 1.1;
+            return base.Payment() + PERCENT_ADDITIONAL * AdditionalCharge;
+        }
+    }
+}
